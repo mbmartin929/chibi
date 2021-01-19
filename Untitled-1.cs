4b9a36c3910da0b46fe2256f0faa874cf5be58ac
mbmartin929 <martinlouie56@gmail.com>
2021-01-19 15:10:03 +0800

    asde

---
+++ Untitled-1.cs
@@ -0,0 +1,45 @@
+
+
+
+
+1. ======================
+int answer = 0;
+
+for (int i = 0; i <= 70; i++)
+{
+    answer += i;
+}
+==========================
+
+2. ======================
+int a,b,c;
+int big = 0;
+
+if (big <= a)
+{
+    big = a;
+}
+if (big <= b)
+{
+    big = b;
+}
+if (big <= c)
+{
+    big = c;
+}
+============================
+
+3. =========================
+int[] array;
+int[] ascendArray;
+int lowest = 0;
+int x = 0;
+
+for(int i = 0; i < array.length; i++)
+{
+    if (array[i] <= array[i+1])
+    {
+        x = array[i]
+    }
+}
+
